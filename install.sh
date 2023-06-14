9a2b95277c5dcc20de5e55ada1337d3efcfcf2c4
Thierry Kauffmann <thierry.kauffmann@arawa.fr>
2023-06-14 14:53:02 +0200

    Update install.sh for multi-tenancy

--- install.sh
+++ install.sh
@@ -7,38 +7,50 @@ echo -e "\e[33mcp {fullchain.pem,privkey.pem} data/arawa/cert/\e[39m"
 printf "\n"
 
 # First, run the .env creation wizard
-bash ./init-scripts/createEnv.sh
+./init-scripts/createEnv.sh
+cat .env
+
+echo -e "\e[33mPlease check generated .env file (above). Continue ? (Y/n)\e[39m"
+read proceed
+[[ "$proceed" == "n" ]] || [[ "$proceed" == "N" ]] && { echo "Aborting"; exit 1; }
+
+source .env
 
 # chmod the log directory
 chmod -R 777 log/
+
+
+if [[ "$MULTITENANCY_ENABLED" == true]]; then
+  echo -e "\e[33mMulti-tenancy enabled : updating nginx config.\e[39m"
+  sed -i -e 's#server_name.*#server_name ~^(.*\\.|)$NGINX_HOSTNAME$;#' data/proxy/nginx/sites.template*
+fi
 
 # Let's encrypt
 echo -e "\e[36mUse Let's Encrypt ?(y/N) :\e[39m"
 read RUN_LETSENCRYPT
 if [[ "$RUN_LETSENCRYPT" == "y" ]] || [[ "$RUN_LETSENCRYPT" == "Y" ]]; then
-  bash ./init-scripts/init-letsencrypt.sh
-fi
-
-# Create DB if needed
-RECORDING_DISABLED=$(grep RECORDING_DISABLED .env | cut -d '=' -f2)
-if [[ "$RECORDING_DISABLED" == false ]]; then
-	ARG_SYSTEMD=" storage"
-  	echo -e "\e[36mCreate the DB with the following command:\e[39m"
-	echo -e "\e[36mdocker exec -it scalelite-api bin/rake db:setup/\e[39m"
-else
-	ARG_SYSTEMD=""
+  if [[ "$MULTITENANCY_ENABLED" == true]]; then
+    echo -e "\e[33mMulti-tenancy enabled : make sure you selected certbot/dns-ovh as Docker image and populated .ovhapi. Proceed now ? (Y/n)\e[39m"
+	read proceed
+	if [[ "$proceed" == "y" ]] || [[ "$proceed" == "Y" ]] || [[ -z $proceed ]]; then
+      ./init-scripts/init-letsencrypt_dns-ovh.sh
+	else
+	  exit 1
+	fi
+  else
+   ./init-scripts/init-letsencrypt.sh
+  fi
 fi
 
 # Make systemD
 echo -e "\e[36mCreate systemd daemon ?(Y/n) :\e[39m"
 read MK_SYSTEMD
 if [[ "$MK_SYSTEMD" == "y" ]] || [[ "$MK_SYSTEMD" == "Y" ]] || [[ -z $MK_SYSTEMD ]]; then
-  bash ./init-scripts/arawa-create-systemd.sh $ARG_SYSTEMD
+  ./init-scripts/arawa-create-systemd.sh
 fi
 
 # Final test
 sleep 5
 
-URL_HOST=$(grep URL_HOST .env | cut -d '=' -f2)
 echo -e "\e[36mTesting the API...\e[39m"
 curl https://$URL_HOST/bigbluebutton/api/
